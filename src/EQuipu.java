6ff657ebe2fc683eb06965640ae3696e43881cec
Dan-elC <root@localhost.localdomain>
2019-03-31 13:02:43 -0500

    2.0

--- src/EQuipu.java
+++ src/EQuipu.java
@@ -1,1 +1,16 @@
-public class EQuipu
+import java.util.*;
+
+
+public class EQuipu{
+	private List<Team> teams;
+
+	public EQuipu(){
+		teams = new ArrayList<Team>();
+	}
+	public List<Team> getTeams(){
+		return teams;
+	}
+	public void setTeams(List<Team> teams){
+		this.teams = teams;
+	}
+}
