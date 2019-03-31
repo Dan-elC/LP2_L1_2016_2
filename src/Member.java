bc76b61d2f8a6fa68b9ba3eb970094f955054222
Dan-elC <danel.campana@pucp.pe>
2019-03-31 13:38:32 -0500

    1.0

---
+++ src/Member.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Member{
+
+	int code;
+	String name;
+	Date birthday;
+	String address;
+	String email;
+	char gender;
+
+	public Member(int code,	String name, Date birthday,	String address,	String email, char gender){
+		
+	}
+}
