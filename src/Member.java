abf10e0ed022eae53b235036a76f8f8af0b4a5c3
Dan-elC <danel.campana@pucp.pe>
2019-03-31 13:54:12 -0500

    1.0

--- src/Member.java
+++ src/Member.java
@@ -13,8 +13,39 @@ public class Member{
 		this.name=name;
 		this birthday = birthday;
 		this.addess=addres;
+		this.email=email;
+		this.gender=gender;
 		code++;
 	}
 
-
+	public getName(){
+		return name;
+	}
+	public setName(String name){
+		this.name = name;
+	}
+	public getBirthday(){
+		return birthday;
+	}
+	public setBirthday(Date birthday){
+		this.birthday = birthday;
+	}
+	public getAddress(){
+		return address;
+	}
+	public setAddress(String address){
+		this.address=address;
+	}
+	public getEmail(){
+		return email;
+	}
+	public setAddress(String email){
+		this.email=email;
+	}
+	public getGender(){
+		return gender;
+	}
+	public setGender(char gender){
+		this.gender=gender;
+	}
 }
